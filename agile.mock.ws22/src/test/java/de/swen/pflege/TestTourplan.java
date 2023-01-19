4f0da51429140c64880f786c9d6b31442b8b3cac
Florian S. <florian.soeder@htwg-konstanz.de>
2023-01-19 12:26:45 +0100

    Task 1: Implemented Test

--- agile.mock.ws22/src/test/java/de/swen/pflege/TestTourplan.java
+++ agile.mock.ws22/src/test/java/de/swen/pflege/TestTourplan.java
@@ -14,7 +14,50 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 public class TestTourplan {
-	// TODO 
-	
+
+	// TEST TASK 1
+
+	@Test(timeout = 1000)
+	public void testConstructorNameLengthThree() {
+		String name = new String();
+		for (int i = 0; i < 3; i++) {
+			name += "A";
+		}
+		Tourplan out = new Tourplan(name, new CentralClientFinder(), new SapService());
+		assertEquals(name, out.getName());
+	}
+
+	@Test(timeout = 1000)
+	public void testConstructorNameLengthThirtyone() {
+		String name = new String();
+		for (int i = 0; i < 31; i++) {
+			name += "A";
+		}
+		Tourplan out = new Tourplan(name, new CentralClientFinder(), new SapService());
+		assertEquals(name, out.getName());
+	}
+
+	@Test(timeout = 1000, expected = IllegalArgumentException.class)
+	public void testConstructorNameNull() {
+		new Tourplan(null, new CentralClientFinder(), new SapService());
+	}
+
+	@Test(timeout = 1000, expected = IllegalArgumentException.class)
+	public void testConstructorNameLengthTwo() {
+		String name = new String();
+		for (int i = 0; i < 2; i++) {
+			name += "A";
+		}
+		new Tourplan(name, new CentralClientFinder(), new SapService());
+	}
+
+	@Test(timeout = 1000, expected = IllegalArgumentException.class)
+	public void testConstructorNameLengthThirtytwo() {
+		String name = new String();
+		for (int i = 0; i < 32; i++) {
+			name += "A";
+		}
+		new Tourplan(name, new CentralClientFinder(), new SapService());
+	}
 
 }
